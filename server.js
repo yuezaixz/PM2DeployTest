5017d529b5e26de7e089b55d9d175b49fe47946c
吴迪玮 <xiao303178394@gmail.com>
2018-06-05 20:50:21 +0800

    modify hello content

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3008;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello David\n');
 });
 
 server.listen(port, hostname, () => {
